188f537468af7c9931c9afa379d2f901c3598606
santalife <kaivert@hotmail.com>
2022-03-28 23:42:13 +0800

    admin route layout done

--- Routes/admin.js
+++ Routes/admin.js
@@ -2,6 +2,11 @@ const express = require('express');
 const { authRole } = require('../services/auth');
 const router = express.Router();
 
+router.all('/*', function (req, res, next) {
+    req.app.locals.layout = 'AdminLayout'; // set your layout here
+    next(); // pass control to the next handler
+});
+
 router.get('/manage/items/create', authRole("admin"), (req, res) => {
     res.render('admin/createItem');
 });
@@ -11,7 +16,7 @@ router.post('/manage/items/create', async function (req, res){
 });
 
 router.get('/index', (req, res) => {
-    res.render('Admin/index' , {layout: 'AdminLayout'});
+    res.render('Admin/index');
 });
 
 module.exports = router;
