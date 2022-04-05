fd297585390f0a3648ce4627e2026700b1ea98e6
NIQIZ <95207856+NIQIZ@users.noreply.github.com>
2022-04-05 14:15:11 +0800

    retrieve items in admin backed

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const { authRole } = require('../services/auth');
 const router = express.Router();
-const item = require('../Models/Item');
 const Item = require('../Models/Item');
+const moment = require('moment');
+
 
 router.all('/*', function (req, res, next) {
     req.app.locals.layout = 'AdminLayout'; // set your layout here
@@ -17,18 +18,38 @@ router.get('/manage/items/create', (req, res) => {
     res.render('admin/createItem');
 });
 
-// router.post('/manage/items/create', async function (req, res){
-
-// });
-
-router.get('/manage/items/retrieve', (req, res) => {
-    
-    res.render('admin/retrieveItem');
+router.post('/manage/items/create', async function (req, res){
+    let {name, description, price, status, stock} = req.body;
+    let item = await Item.create({
+        name,
+        description,
+        price,
+        post_date: moment(),
+        last_updated: moment(),
+        status,
+        stock,
+        sold: false
+    });
+    res.redirect('/admin/manage/items/retrieve');
 });
 
-router.get('/manage/items/update', (req, res) => {
-    let items = Item;
-    res.render('admin/updateItem', {Item});
+router.get('/manage/items/retrieve', async function (req, res) {
+    let items = await Item.findAll({
+        order: [['last_updated', 'DESC']],
+        raw: true
+    });
+    res.render('admin/retrieveItem', {items});
+});
+
+router.get('/manage/items/update/:id', async function (req, res) {
+    let item = await Item.findOne({
+        where:{
+            id: req.params.id
+        },
+        raw: true
+    });
+    console.log(item);
+    res.render('admin/updateItem', {item});
 });
 
 module.exports = router;
