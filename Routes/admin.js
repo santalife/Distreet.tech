6dc806ec3db8b1f2ea1152c1cec28700c84429b6
NIQIZ <95207856+NIQIZ@users.noreply.github.com>
2022-04-05 13:03:30 +0800

    create item

--- Routes/admin.js
+++ Routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { authRole } = require('../services/auth');
 const router = express.Router();
+const item = require('../Models/Item');
+const Item = require('../Models/Item');
 
 router.all('/*', function (req, res, next) {
     req.app.locals.layout = 'AdminLayout'; // set your layout here
@@ -15,10 +17,18 @@ router.get('/manage/items/create', (req, res) => {
     res.render('admin/createItem');
 });
 
-router.post('/manage/items/create', async function (req, res){
+// router.post('/manage/items/create', async function (req, res){
 
+// });
+
+router.get('/manage/items/retrieve', (req, res) => {
+    
+    res.render('admin/retrieveItem');
 });
 
-
+router.get('/manage/items/update', (req, res) => {
+    let items = Item;
+    res.render('admin/updateItem', {Item});
+});
 
 module.exports = router;
